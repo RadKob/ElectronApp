8cd88491f59872cdca1d167c9e8031b4a90c377c
Radek Kobus <radek109@o2.pl>
2023-05-28 22:43:26 +0200

    Add login page

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 "# ElectronApp" 
 "# 28.05 - create repository" 
 "# 28.05 - create basic files of project" 
+"# 28.05 - login page" 
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 400,
+	useContentSize: true,
+	frame: false,
     webPreferences: {
       nodeIntegration: true
     }
